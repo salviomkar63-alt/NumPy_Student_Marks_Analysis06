d54f56368ed0275168d300ed896adec76b36b85f
salviomkar63-alt <salviomkar63@gmail.com>
2025-11-02 00:20:32 +0530

    Add files via upload

---
+++ mini_project/mini_projecr.py
@@ -0,0 +1,37 @@
+'''Tasks:
+Take input (or random) marks of students (say 10x5 matrix → meanstudents × subjects).
+Calculate total marks, average, and grade for each student.
+Find the topper and subject-wise highest scores.
+Display class average and subject averages.
+Save the data to a .csv file using np.savetxt()
+💡 Bonus: Add a small menu-based interface using Python (input() statements).
+When finished, upload it to GitHub → folder name: NumPy_StudentMarksAnalyzer.
+'''
+import numpy as np
+
+marks = np.array([
+    [78, 85, 90, 88, 92],
+    [70, 60, 65, 72, 68],
+    [95, 90, 92, 98, 88],
+    [50, 55, 60, 58, 52],
+    [80, 82, 78, 85, 88]
+])
+
+flat = marks.flatten()
+average = (np.mean(marks,axis=1))
+percentage = (np.sum(marks,axis=1))
+print("-----student marks analysis start-----")
+
+print(f"The highest percentage of among all student {np.max(average)} ,(student{np.argmax(average)+1})")   #highest percentage of student
+
+print(f"The minmum percentage of among  all student {np.min(average)} ,(student{np.argmin(average)+1})")    #minimum percentage of student
+
+sub = np.max(marks,axis=0)
+
+print(f"Highest marks in each subject: {sub} ")    #highest marks in each sub
+print(f"Class average : {np.mean(marks)}")         #class average 
+each_sub_avg = np.mean(marks,axis=0)          
+print(f"The average marks in each subject is: {each_sub_avg}")   #average in each subject 
+
+
+print("-----student analysis completed-----")
